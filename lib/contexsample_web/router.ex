9ff0d71c846b02c164d632bba59fab2a6a787413
John Jessop <jessop.john@gmail.com>
2020-02-11 13:36:36 +0800

    Update to latest Contex. Add basic logging

--- lib/contexsample_web/router.ex
+++ lib/contexsample_web/router.ex
@@ -1,17 +1,22 @@
 defmodule ContexSampleWeb.Router do
+  require Logger
   use ContexSampleWeb, :router
+
+  def basic_log(conn, _opts) do
+    # live routes don't emit logs with request path
+    ip = conn.remote_ip |> Tuple.to_list() |> Enum.join(".")
+    Logger.info("Requested #{conn.request_path} from #{ip}")
+    conn
+  end
 
   pipeline :browser do
     plug :accepts, ["html"]
+    plug :basic_log
     plug :fetch_session
     plug :fetch_flash
     plug Phoenix.LiveView.Flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
   end
 
   scope "/", ContexSampleWeb do
@@ -20,15 +25,11 @@ defmodule ContexSampleWeb.Router do
     get "/", PageController, :index
     get "/faq", PageController, :faq
 
-    live "/barcharts", BarChartLive
+    live "/barcharts", BarChartLive, session: [:remote_ip]
     live "/barchart_timer", BarChartTimer
     live "/sparklines", SparklineLive
     live "/gantt", GanttLive
     live "/point", PointPlotLive
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ContexSampleWeb do
-  #   pipe_through :api
-  # end
 end

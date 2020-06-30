54e7f546851e8a40d1b9f0b7ba7828459dd43891
unknown <jessop.john@gmail.com>
2020-06-30 16:22:51 +0800

    Another webpack fix attempt - deploy is breaking

--- bin/release-build.sh
+++ bin/release-build.sh
@@ -7,8 +7,10 @@ mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-npm install --prefix ./assets
-npm run deploy --prefix ./assets
+cd assets
+npm install --prefix 
+npm run deploy --prefix
+cd ..
 mix phx.digest
 
 # Remove the existing release directory and build the release

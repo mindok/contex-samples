f24c478143349947b14276c8c9af04cfa5bdd9c3
Ivan Uemlianin <ivan@llaisdy.com>
2024-03-01 15:13:42 +0000

    config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
@@ -7,19 +7,56 @@
 # General application configuration
 import Config
 
-config :contexsample,
-  ecto_repos: [Contexsample.Repo]
+config :contex_sample,
+  ecto_repos: [ContexSample.Repo],
+  generators: [timestamp_type: :utc_datetime]
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") || "NmtK9GGA7C88oSZdW8oW73MGNgzufNXSlTYig7krCriszdZBdJezs39ZjFbZ5QFW"
+  System.get_env("SECRET_KEY_BASE") ||
+    "NmtK9GGA7C88oSZdW8oW73MGNgzufNXSlTYig7krCriszdZBdJezs39ZjFbZ5QFW"
 
 # Configures the endpoint
-config :contexsample, ContexSampleWeb.Endpoint,
+config :contex_sample, ContexSampleWeb.Endpoint,
   url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
   secret_key_base: secret_key_base,
-  render_errors: [view: ContexSampleWeb.ErrorView, accepts: ~w(html json)],
-  pubsub_server: Contexsample.PubSub,
+  render_errors: [
+    formats: [html: ContexSampleWeb.ErrorHTML, json: ContexSampleWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: ContexSample.PubSub,
   live_view: [signing_salt: "am2/tatn8cjd6Q+A/yZqyA0naK/NVIRb"]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :contex_sample, ContexSample.Mailer, adapter: Swoosh.Adapters.Local
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  contex_sample: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.4.0",
+  contex_sample: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -31,4 +68,4 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,33 @@
 import Config
 
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :contex_sample, ContexSample.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "contex_sample_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :contexsample, ContexSampleWeb.Endpoint,
-  http: [port: 4002],
+config :contex_sample, ContexSampleWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "G48f8r/Wec4xrW0TbF8SVGnZTNSycYjEPKANY2fWIe/gZ69NHIcVafWPdITBUn/C",
   server: false
 
+# In test we don't send emails.
+config :contex_sample, ContexSample.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
